dcf6fa946bef04ef663588ba3b9b1a4d717d0405
Alley M. Carvalho <alleycarvalho@gmail.com>
2020-04-15 01:44:34 -0300

    feat(transactions): list transactions and balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -15,24 +15,51 @@ interface CreateTransactionDTO {
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
   public create({ title, value, type }: CreateTransactionDTO): Transaction {
     const transaction = new Transaction({ title, value, type });
 
+    this.setBalance({ value, type });
     this.transactions.push(transaction);
 
     return transaction;
+  }
+
+  private setBalance({
+    value,
+    type,
+  }: Omit<CreateTransactionDTO, 'title'>): Balance {
+    if (type === 'income') {
+      this.balance.income += value;
+    } else {
+      if (this.balance.total - value < 0) {
+        throw Error('Insufficient balance for this transaction');
+      }
+
+      this.balance.outcome += value;
+    }
+
+    this.balance.total = this.balance.income - this.balance.outcome;
+
+    return this.balance;
   }
 }
 
